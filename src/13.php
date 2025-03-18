0c59b828f27de341f689b2a20b3ecf5768d55b9d
cristalinarui <cristalinarui@gmail.com>
2025-03-18 08:20:17 +0000

    Add 13.php

---
+++ src/13.php
@@ -0,0 +1,15 @@
+<?php
+
+$num1 = rand(1, 20);
+$num2 = rand(1, 20);
+$sum = $num1 + $num2;
+$sub = $num1 - $num2;
+$mul = $num1 * $num2;
+$div = $num1 / $num2;
+
+echo "The sum is: $sum";
+echo "The subtraction is: $sub";
+echo "The multiplication is: $mul";
+echo "The division is: $div";
+
+?>
