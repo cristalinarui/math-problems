eca8e0bdab97874f6b771868de22305f560892d4
cristalinarui <cristalinarui@gmail.com>
2025-04-05 04:17:21 +0000

    Add 23.php

---
+++ src/23.php
@@ -0,0 +1,13 @@
+<?php 
+// Problem 1: Addition
+echo "Adding 2 + 3 = 5";
+
+// Problem 2: Subtraction
+echo "Subtracting 4 - 1 = 3";
+
+// Problem 3: Multiplication
+echo "Multiplying 7 * 6 = 42";
+
+// Problem 4: Division
+echo "Dividing 8 / 2 = 4";
+?>
