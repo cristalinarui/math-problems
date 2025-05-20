3a4cb76770927b673d6f6a8bae19d2fcd76d9793
cristalinarui <cristalinarui@gmail.com>
2025-05-20 00:45:35 +0000

    Add 35.php

---
+++ src/35.php
@@ -0,0 +1,15 @@
+<?php
+
+// Define variables and constants
+$variables = array(
+    "x" => 5,
+    "y" => 10,
+);
+
+// Perform calculation
+$result = $variables["x"] + $variables["y"];
+
+// Display the result
+echo "The sum of x and y is: " . $result;
+
+?>
